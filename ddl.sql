07135fa392fc7a0e0dd5bde6406c18f087394c08
mag98404 <mag98404@uga.edu>
2025-11-21 14:08:23 -0500

    Updated ddl file with 2 new tables

--- ddl.sql
+++ ddl.sql
@@ -13,3 +13,25 @@ CREATE TABLE if not exists games(
   ,other_sales  NUMERIC(5,2)
   ,global_sales NUMERIC(5,2)
 );
+
+create table if not exists user (
+    userId int auto_increment,
+    username varchar(255) not null,
+    password varchar(255) not null,
+    firstName varchar(255) not null,
+    lastName varchar(255) not null,
+    primary key (userId),
+    unique (username),
+    constraint userName_min_length check (char_length(trim(userName)) >= 2),
+    constraint firstName_min_length check (char_length(trim(firstName)) >= 2),
+    constraint lastName_min_length check (char_length(trim(lastName)) >= 2)
+);
+
+create table if not exists follows (
+    followingId int not null,
+    followedId int not null,
+    primary key (followingId, followedId),
+    key followedId (followedId),
+    constraint follows_ibfk_1 foreign key (followingId) references user (userId),
+    constraint follows_ibfk_2 foreign key (followedId) references user (userId)
+);
