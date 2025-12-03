0740f1867de3ecab788eec7b96b0678a08f03112
mag98404 <mag98404@uga.edu>
2025-12-03 10:21:00 -0500

    Added TEMP avatars, added login + registration functionality

--- src/main/java/com/csci4370/finalproject/controllers/HomeController.java
+++ src/main/java/com/csci4370/finalproject/controllers/HomeController.java
@@ -1,13 +1,34 @@
 package com.csci4370.finalproject.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.csci4370.finalproject.services.UserService;
 
 @Controller
+@RequestMapping
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "home_page"; // Will look for src/main/resources/templates/home.mustache
+    private final UserService userService;
+    @Autowired
+    public HomeController(UserService userService) {
+        this.userService = userService;
     }
+
+    @GetMapping
+    public ModelAndView webpage(@RequestParam(name = "error", required = false) String error) {
+        // See notes on ModelAndView in BookmarksController.java.
+        ModelAndView mv = new ModelAndView("home_page");
+
+        if (!userService.isAuthenticated()) {
+            mv.setViewName("redirect:/login");
+            return mv;
+        }
+        return mv;
+    }
+
 }
