5223ab72a434cd7e151a6dc1cf4c733f096e4c2c
Jonah Criswell <jvc.criswell@gmail.com>
2025-12-03 14:59:02 -0500

    Search Games done, need to adjust schema next

---
+++ src/main/java/com/csci4370/finalproject/services/GamesService.java
@@ -0,0 +1,57 @@
+package com.csci4370.finalproject.services;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.csci4370.finalproject.models.Game;
+
+@Service
+public class GamesService {
+
+   private final DataSource dataSource;
+
+   @Autowired
+   public GamesService(DataSource dataSource) {
+      this.dataSource = dataSource;
+   }
+
+   public List<Game> searchGameByTitle(String name) {
+      List<Game> games = new ArrayList<>();
+      if (name == null || name.isEmpty()) {
+         return games;
+      }
+      final String sql = "select * from games where name like ?";
+      try (Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+         pstmt.setString(1, "%" + name + "%");
+
+         try (ResultSet rs = pstmt.executeQuery()) {
+            while (rs.next()) {
+               String gameId = rs.getString("game_id");
+               String gameTitle = rs.getString("name");
+               int year = rs.getInt("year");
+               String genre = rs.getString("genre");
+               String platform = rs.getString("platform");
+               String publisher = rs.getString("publisher");
+               games.add(new Game(gameId, gameTitle, year, platform, genre, publisher));
+            }
+         } catch (SQLException e) {
+            e.printStackTrace();
+         }
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
+      return games;
+   }
+}
