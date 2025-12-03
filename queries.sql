664e11c3aed0cb4716559f441991f34cdbe74f8a
Christopher McIntosh <132169735+chrismtop@users.noreply.github.com>
2025-12-03 14:51:39 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -14,11 +14,16 @@ SELECT * FROM games WHERE FLOOR(year / 10) = FLOOR(? / 10) ORDER BY global_sales
 
 -- Friends / Follows Page
 
+-- Games Page (for browsing and searching games)
+-- Using the search bar, get games with the title that the user inputs
+
+
 -- Game Page (for whatever game is being looked at by the user)
 -- Get information about the specific game
 SELECT * FROM games WHERE game_id = ?;
 -- Get the game info and reviews for the game
 SELECT * FROM games g LEFT JOIN reviews r ON g.game_id = r.review_of WHERE g.game_id = ?;
+
 
 -- Reviews
 -- Get the info for a review and its comments
@@ -26,7 +31,8 @@ SELECT * FROM reviews r LEFT JOIN review_comments c ON r.review_id = c.posted_on
 
 -- Profile Page
 -- Get user info and all their reviews
-SELECT * FROM user u LEFT JOIN reviews r on u.user_id = r.review_by WHERE u.user_id = ?;
+SELECT * FROM user u LEFT JOIN reviews r on u.userId = r.review_by WHERE u.userId = ?;
+
 
 -- Bookmark tab: shows games you want to play, and basic info about each game
 -- Retrieves games that the user has bookmarked 
