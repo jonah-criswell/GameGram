4e75511ee12ec882b0c2528c9863d54ae751c641
Christopher McIntosh <132169735+chrismtop@users.noreply.github.com>
2025-12-04 13:59:07 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -3,11 +3,11 @@
 
 -- Homepage 
 -- Finds the 10 most popular games worldwide based on sales. 
-SELECT g.game_id, g,name, g,genre, SUM(p.global_sales) AS total_global_sales FROM games g 
-JOIN platforms p ON g.game_id = p.game_id GROUP BY g.game_id, g.name, g.genre ORDER BY total_global_sales DESC;
+SELECT g.*, SUM(p.global_sales) AS total_global_sales FROM games g 
+JOIN platforms p ON g.game_id = p.game_id GROUP BY g.game_id ORDER BY total_global_sales DESC LIMIT 10;
 -- Finds the 10 most popular games that have a particular platform
-WITH popular_games as (SELECT g.game_id, g,name, g,genre, SUM(p.global_sales) AS total_global_sales FROM games g 
-JOIN platforms p ON g.game_id = p.game_id GROUP BY g.game_id, g.name, g.genre ORDER BY total_global_sales DESC)
+WITH popular_games as (SELECT g.*, p.*, SUM(p.global_sales) AS total_global_sales FROM games g 
+JOIN platforms p ON g.game_id = p.game_id GROUP BY g.game_id, p.game_id ORDER BY total_global_sales DESC)
 SELECT * FROM popular_games WHERE platform = ? ORDER BY total_global_sales DESC LIMIT 10;
 -- Finds the 10 most popular games that have a particular publisher
 WITH popular_games as (SELECT g.game_id, g,name, g,genre, SUM(p.global_sales) AS total_global_sales FROM games g 
@@ -24,7 +24,7 @@ SELECT *, COUNT(fr.review_id) as r_count FROM games g JOIN followed_reviews fr O
 GROUP BY g.game_id ORDER BY r_count DESC LIMIT 10;
 -- Finds the users not followed by the user
 WITH followed_users as (SELECT f.followedId as friend_id FROM follows f WHERE f.followingId = ?)
-SELECT * FROM user u WHERE u.user_id NOT IN (SELECT friend_id FROM followed_users);
+SELECT * FROM user u WHERE u.user_id NOT IN (SELECT friend_id FROM followed_users) AND u.user_id <> ?;
 
 -- Games Page (for browsing and searching games)
 -- Using the search bar, get games with the title that the user inputs
