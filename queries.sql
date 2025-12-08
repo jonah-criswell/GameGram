5d729134eb91ebbac936c4e1eb172680122d26c6
Christopher McIntosh <132169735+chrismtop@users.noreply.github.com>
2025-12-08 14:45:59 -0500

    added extra deliverables and updated queries

--- queries.sql
+++ queries.sql
@@ -1,64 +1,27 @@
 -- every query used in the code to retrieve stuff on the site goes here, 
 -- along with a comment explaining what each one does and where
 
--- homepage get 10 most popular games query
-SELECT
-    g.game_id,
-    g.name,
-    g.genre,
-    GROUP_CONCAT(DISTINCT p.platform ORDER BY p.platform) AS platforms,
-    GROUP_CONCAT(DISTINCT p.publisher ORDER BY p.publisher) AS publishers,
-    GROUP_CONCAT(DISTINCT p.year ORDER BY p.year) AS years,
-    SUM(p.na_sales) AS total_na_sales,
-    SUM(p.eu_sales) AS total_eu_sales,
-    SUM(p.jp_sales) AS total_jp_sales,
-    SUM(p.other_sales) AS total_other_sales,
-    SUM(p.global_sales) AS total_global_sales
-FROM games g
-JOIN platforms p ON g.game_id = p.game_id
-GROUP BY g.game_id, g.name, g.genre
-ORDER BY total_global_sales DESC LIMIT 10;
-
-
--- game summary creation to run after inserting all the data
-CREATE VIEW game_summary AS
-SELECT
-    g.game_id,
-    g.name,
-    g.genre,
-    GROUP_CONCAT(DISTINCT p.platform ORDER BY p.platform) AS platforms,
-    GROUP_CONCAT(DISTINCT p.publisher ORDER BY p.publisher) AS publishers,
-    GROUP_CONCAT(DISTINCT p.year ORDER BY p.year) AS years,
-    SUM(p.global_sales) AS total_global_sales
-FROM games g
-JOIN platforms p ON g.game_id = p.game_id
-GROUP BY g.game_id, g.name, g.genre;
-
--- User registration
+-- User registration and authentication
 -- insert a new user and their information into the database
-insert into user (username, password, firstName, lastName) values (?, ?, ?, ?)
+insert into user (username, password, firstName, lastName) values (?, ?, ?, ?);
+-- Authentication
+select * from user where username = ?;
+-- Get the info of a specific user 
+select * from user where userId = ?;
+-- Get the users a user follows
+SELECT * FROM user u LEFT JOIN follows f ON u.userId = f.followedId WHERE f.followingId = ?;
 
 -- Homepage 
--- Finds the 10 most popular games worldwide based on sales. 
-SELECT * FROM game_summary ORDER BY total_global_sales DESC LIMIT 10;
+-- Get the 10 most popular games based on global sales
+SELECT g.game_id, g.name, g.genre, p.platform, p.year, p.publisher,
+                           p.na_sales, p.eu_sales, p.jp_sales, p.other_sales, p.global_sales
+                    FROM games g
+                    JOIN platforms p ON g.game_id = p.game_id
+                    ORDER BY p.global_sales DESC
+                    LIMIT 10;
 -- Finds the users not followed by the user
 WITH followed_users as (SELECT f.followedId as friend_id FROM follows f WHERE f.followingId = ?)
 SELECT * FROM user u WHERE u.user_id NOT IN (SELECT friend_id FROM followed_users) AND u.user_id <> ?;
--- Finds the 10 most popular games among the user's friends
-WITH followed_users AS (SELECT f.followedId AS friend_id FROM follows f WHERE f.followingId = ?),
-followed_reviews AS (SELECT r.review_id, r.review_of FROM reviews r JOIN followed_users fu ON r.review_by = fu.friend_id)
-SELECT gs.*, COUNT(fr.review_id) AS r_count FROM game_summary gs LEFT JOIN followed_reviews fr ON fr.review_of = gs.game_id
-GROUP BY gs.game_id ORDER BY r_count DESC LIMIT 10;
-
--- - don't know if we're actually using these - ---------------------------------------------------------------------------------------
--- Finds the 10 most popular games that have a particular platform
-SELECT * FROM game_summary WHERE FIND_IN_SET(?, platforms) ORDER BY total_global_sales DESC LIMIT 10;
--- Finds the 10 most popular games that have a particular publisher
-SELECT * FROM game_summary WHERE FIND_IN_SET(?, publishers) ORDER BY total_global_sales DESC LIMIT 10;
--- Finds the 10 most popular games that were published in a particular decade
-SELECT * FROM game_summary WHERE EXISTS (SELECT 1 FROM platforms p WHERE p.game_id = game_summary.game_id 
-AND FLOOR(p.year / 10) = FLOOR(? / 10)) ORDER BY total_global_sales DESC LIMIT 10;
-----------------------------------------------------------------------------------------------------------------------------------------
 
 -- Games Page (for browsing and searching games)
 -- Using the search bar, get games with the title that the user inputs
@@ -71,17 +34,31 @@ SELECT AVG(rating) as avg_rating FROM reviews WHERE review_of = ?;
 
 -- Profile Page
 -- Get user info and all their reviews 
-SELECT * FROM user u LEFT JOIN reviews r on u.userId = r.review_by WHERE u.userId = ?;
--- Get the user's average review rating
-SELECT AVG(rating) as avg_rating FROM reviews WHERE review_by = ?;
+SELECT 
+                 r.reviewId, r.content, DATE_FORMAT(r.postDate, '%M %d, %Y') as formattedDate,
+                 r.game_id, r.hoursPlayed, r.reviewRating, r.heartsCount, r.commentsCount, 
+                 r.isHearted, r.isBookmarked, 
+                 u.userId, u.firstName, u.lastName, 
+                 g.name as gameName 
+                 FROM review r 
+                 JOIN user u ON r.userId = u.userId 
+                 JOIN games g ON r.game_id = g.game_id 
+                 WHERE u.userId = ? 
+                 ORDER BY r.postDate DESC;
 
 -- Friends / Follows Page
 -- Get all users that the current user follows
 SELECT * FROM user u LEFT JOIN follows f ON u.userId = f.followedId WHERE f.followingId = ?; 
+-- When a user follows another user, update the database to reflect this
+INSERT INTO follows (followingId, followedId) VALUES (?, ?);
+-- When a user unfollows another user 
+DELETE FROM follows WHERE followingId = ? AND followedId = ?;
 
 -- Reviews
 -- Insert a review into the database when a user makes a review
---INSERT INTO reviews ()
+insert into review (game_id, hoursPlayed, content, reviewRating, postDate, userId) values (?, ?, ?, ?, now(), ?);
+-- Get all reviews for a game
+SELECT * FROM review WHERE game_id = ?;
 
 -- Hearts and Comments for Reviews
 -- When a user hearts a post this stores that in the database. Used on any page displaying posts 
@@ -95,8 +72,3 @@ INSERT INTO review_comments (comment_id, comment_text, comment_date, posted_on, 
 -- Access a review's number of comments
 SELECT COUNT(comment_id) as comment_count FROM review_comments WHERE posted_on = ?;
 
-
-
-
--- Bookmark tab: shows games you want to play, and basic info about each game
--- Retrieves games that the user has bookmarked 
---
+++ readme.txt
@@ -0,0 +1,13 @@
+Group 16
+
+Jonah Criswell - 
+Mason Gindlesperger - 
+Chris McIntosh - 
+Aidan Murray - 
+Joey Vos - 
+
+Include all the technologies you used including 3rd party libraries.
+
+Include the database name, database username and the password you used for the JDBC connection.
+
+Provide three test username and password pairs from your demo data.
